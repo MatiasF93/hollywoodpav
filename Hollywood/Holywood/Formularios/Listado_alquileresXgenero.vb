3f04aeff664628475947c9c58d6fe2a2db81ec76
LuMoll22 <lucianamoll@hotmail.com>
2015-10-27 08:25:25 -0300

    Le agregue la s a genero, no me di cuenta que en la BD estaba como generos

--- Hollywood/Holywood/Formularios/Listado_alquileresXgenero.vb
+++ Hollywood/Holywood/Formularios/Listado_alquileresXgenero.vb
@@ -2,18 +2,18 @@
     Dim acceso As New Acceso_Datos
     Private Sub Listado_alquileresXgenero_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         Dim sql As String = ""
-        sql = "SELECT  peliculas.nombre as nombre_alquiler,  genero.descripcion as nombre_genero  "
-        sql &= " FROM genero INNER JOIN peliculas ON peliculas.id_genero = genero.id_genero "
-        sql &= " WHERE (peliculas.fecha_alquiler IS NOT NULL) AND (genero.descripcion) LIKE '%" & Me.txt_nombre_genero.Text & "%'"
+        sql = "SELECT  peliculas.nombre as nombre_alquiler,  generos.descripcion as nombre_genero  "
+        sql &= " FROM generos INNER JOIN peliculas ON peliculas.id_genero = generos.id_genero "
+        sql &= " WHERE (peliculas.fecha_alquiler IS NOT NULL) AND (generos.descripcion) LIKE '%" & Me.txt_nombre_genero.Text & "%'"
         DataSet_alquileresXgeneroBindingSource.DataSource = acceso.consultar(sql)
         Me.ReportViewer1.RefreshReport()
     End Sub
 
     Private Sub cmd_buscar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmd_buscar.Click
         Dim sql As String = ""
-        sql = "SELECT  peliculas.nombre as nombre_alquiler,  genero.descripcion as nombre_genero  "
-        sql &= " FROM genero INNER JOIN peliculas ON peliculas.id_genero = genero.id_genero "
-        sql &= " WHERE (peliculas.fecha_alquiler IS NOT NULL) AND (genero.descripcion) LIKE '%" & Me.txt_nombre_genero.Text & "%'"
+        sql = "SELECT  peliculas.nombre as nombre_alquiler,  generos.descripcion as nombre_genero  "
+        sql &= " FROM generos INNER JOIN peliculas ON peliculas.id_genero = generos.id_genero "
+        sql &= " WHERE (peliculas.fecha_alquiler IS NOT NULL) AND (generos.descripcion) LIKE '%" & Me.txt_nombre_genero.Text & "%'"
         DataSet_alquileresXgeneroBindingSource.DataSource = acceso.consultar(sql)
         Me.ReportViewer1.RefreshReport()
     End Sub
